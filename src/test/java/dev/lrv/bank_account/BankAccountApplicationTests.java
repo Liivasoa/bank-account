cab9ff75894a61c046f5c4d87981f28ed856d85c
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2025-01-06 16:13:56 +0100

    Conf : Init springboot project

---
+++ src/test/java/dev/lrv/bank_account/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lrv.bank_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
